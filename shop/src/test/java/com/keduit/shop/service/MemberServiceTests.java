fe66e6c9ba1e834674131f320020564db87ac910
yoonhyoju <112902286+yoonhyoju@users.noreply.github.com>
2024-04-25 17:00:43 +0900

    비밀번호 찾기 controller/ html 수정, 추가 MemberController 수정(usermember삭제) memberForm.html input창수정 취소버튼삭제

--- shop/src/test/java/com/keduit/shop/service/MemberServiceTests.java
+++ shop/src/test/java/com/keduit/shop/service/MemberServiceTests.java
@@ -23,10 +23,10 @@ public class MemberServiceTests {
 
     public Member createMember(){
         MemberFormDTO memberFormDTO = new MemberFormDTO();
-        memberFormDTO.setEmail("test124@Hong.com");
+        memberFormDTO.setEmail("test12411@Hong.com");
         memberFormDTO.setName("홍길동");
 
-        memberFormDTO.setPassword("1234");
+        memberFormDTO.setPassword("12345678");
         return Member.createMember(memberFormDTO, passwordEncoder);
     }
 
@@ -39,7 +39,6 @@ public class MemberServiceTests {
         //비교(각각필드 중복되는지), assert: member.~~와 aveMember~~가 같기를 기대해(충족vs실패)
         assertEquals(member.getEmail(),saveMember.getEmail());
         assertEquals(member.getName(),saveMember.getName());
-        assertEquals(member.getAddress(),saveMember.getAddress());
         assertEquals(member.getPassword(),saveMember.getPassword());
         assertEquals(member.getRole(),saveMember.getRole());
     }
