be693a020a15f534671fc5e7837ec0df5002c5b8
songJinYoung22 <166775363+songJinYoung22@users.noreply.github.com>
2024-04-29 20:29:47 +0900

    ㅇㅇ

--- shop/src/main/java/com/keduit/shop/entity/Address.java
+++ shop/src/main/java/com/keduit/shop/entity/Address.java
@@ -1,6 +1,25 @@
 package com.keduit.shop.entity;
 
-public class Address {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Long id;
+
+
+    private Integer postalCode;
+    private String roadName;
+    private String localAddress;
+
+    private String detailedAddress;
+
+    private  String dong;
+
+    private Boolean check;
 
 }
