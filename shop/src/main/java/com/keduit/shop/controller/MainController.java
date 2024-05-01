1d0cf45676773392349289302bd98557f762118d
songJinYoung22 <166775363+songJinYoung22@users.noreply.github.com>
2024-05-01 20:43:07 +0900

    dd

--- shop/src/main/java/com/keduit/shop/controller/MainController.java
+++ shop/src/main/java/com/keduit/shop/controller/MainController.java
@@ -28,8 +28,8 @@ public class MainController {
         List<Item> items = itemRepository.findAll();
 
         List<ItemImg> itemImgs = itemImgRepository.findAll();
-        System.out.println(items.get(0));
-        System.out.println(itemImgs.get(0).getImgUrl());
+//        System.out.println(items.get(0));
+//        System.out.println(itemImgs.get(0).getImgUrl());
 
         model.addAttribute("items",items);
         model.addAttribute("itemImgs",itemImgs.get(0).getImgUrl());
---
+++ shop/src/test/java/com/keduit/shop/service/OrderServiceTests.java
@@ -0,0 +1,14 @@
+package com.keduit.shop.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+public class OrderServiceTests {
+
+    @Autowired
+    OrderService orderService;
+
+}
