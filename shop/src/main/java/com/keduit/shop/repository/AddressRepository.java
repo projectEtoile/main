bc53e253bd192478f67a82cfbf313af9fb752174
이종원 <nollll@nate.com>
2024-05-03 15:06:02 +0900

    itemDtl에 등록상품 정보 화면에뿌리기 장바구니 삭제 체크아웃 장바구니 상품금액 장바구니 상품 주문 개별처리 장바구니항목표시 장바구니 상품 주문완료시 주문완료된건만 장바구니에서삭제 디벨롭브랜치와병합 orderitem에 size추가 orderitemdto에 size추가 orderservice에 오더리스트가져오는 메서드에 사이즈추가 장바구니에서 상품하나 선택하고 주문하면 주문,주문아이템테이블에 잘저장됨 item테이블에 해당 재고 알맞게 감소 장바구니에서 한개이상 선택하고 주문하면 주문잘되고 각각 해당재고잘감소함 장바구니에서주문할때 재고없는상품은 주문안됨 구매내역에서 주문취소시 재고증가,총재고증가 orders테이블에서 주문상태변경 주소지등록하고 기본배송지를 등록하고 상품주문하면 해당주문이 기본배송지를 담음

--- shop/src/main/java/com/keduit/shop/repository/AddressRepository.java
+++ shop/src/main/java/com/keduit/shop/repository/AddressRepository.java
@@ -5,9 +5,12 @@ import com.keduit.shop.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AddressRepository extends JpaRepository<Address,Long> {
 
     List<Address> findAllByMember(Member member);
+
+    Optional<Address> findByMemberAndSelectAddressTrue(Member member);
 
 }
