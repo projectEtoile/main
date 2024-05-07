f2dd8806b750fc264a0e9b630dd7b1d148699c5c
이종원 <nollll@nate.com>
2024-05-07 18:43:39 +0900

    itemDtl에 등록상품 정보 화면에뿌리기 장바구니 삭제 체크아웃 장바구니 상품금액 장바구니 상품 주문 개별처리 장바구니항목표시 장바구니 상품 주문완료시 주문완료된건만 장바구니에서삭제 디벨롭브랜치와병합 orderitem에 size추가 orderitemdto에 size추가 orderservice에 오더리스트가져오는 메서드에 사이즈추가 장바구니에서 상품하나 선택하고 주문하면 주문,주문아이템테이블에 잘저장됨 item테이블에 해당 재고 알맞게 감소 장바구니에서 한개이상 선택하고 주문하면 주문잘되고 각각 해당재고잘감소함 장바구니에서주문할때 재고없는상품은 주문안됨 구매내역에서 주문취소시 재고증가,총재고증가 orders테이블에서 주문상태변경 주소지등록하고 기본배송지를 등록하고 상품주문하면 해당주문이 기본배송지를 담음 화면에뿌려짐 구매내역 html옮기는작업중 구매내역 추가 구매내역 주문취소 구매내역 선택한것만 주문취소시 주문취소,주문한것 재고감소,취소한상품재고증가 orders테이블에서 주문상태변경 구매내역에서 반품/교환처리 구매내역에서 주문상태가 DELIVERED 인것만 교환/반품 신청가능하게 수정 qanda 등록 상세페이지에서만, 이걸 마이페이지 qanda에 테이블로 뿌려줘야함 뿌려지는것도 수정많이해야함 페이징처리필요 qanda 마이페이지에서 조회가능,페이징처리

--- shop/src/main/java/com/keduit/shop/repository/QandARepository.java
+++ shop/src/main/java/com/keduit/shop/repository/QandARepository.java
@@ -6,11 +6,16 @@ import com.keduit.shop.entity.QandA;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface QandARepository extends JpaRepository<QandA, Long> {
     List<QandA> findAllByItemId(Long itemId);
+
+    List<QandA> findAllByMemberId(Long memberId);
+
+    Page<QandA> findAllByMemberId(Long memberId, Pageable pageable);
 }
 
--- shop/src/main/java/com/keduit/shop/service/QandAService.java
+++ shop/src/main/java/com/keduit/shop/service/QandAService.java
@@ -1,12 +1,12 @@
 package com.keduit.shop.service;
-
 import com.keduit.shop.entity.QandA;
 import com.keduit.shop.repository.QandARepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import javax.transaction.Transactional;
+
 import java.util.List;
 
 @Service
@@ -19,8 +19,21 @@ public class QandAService {
         return qandARepository.findAllByItemId(itemId);
     }
 
+    public List<QandA> getAllQuestions() {
+        return qandARepository.findAll();
+    }
+
     public void save(QandA qanda) {
         qandARepository.save(qanda);
     }
-}
 
+    // QandAService.java
+    public List<QandA> findQuestionsByUserId(Long userId) {
+        return qandARepository.findAllByMemberId(userId);
+    }
+
+    public Page<QandA> findQuestionsByUserId(Long userId, Pageable pageable) {
+        return qandARepository.findAllByMemberId(userId, pageable);
+    }
+
+}
