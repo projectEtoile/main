b4b61375e37990d1bfd57f283903884bf55d20aa
songJinYoung22 <166775363+songJinYoung22@users.noreply.github.com>
2024-05-08 17:33:44 +0900

    카테고리 남음

--- shop/src/main/java/com/keduit/shop/repository/ExchangeReturnRequestRepository.java
+++ shop/src/main/java/com/keduit/shop/repository/ExchangeReturnRequestRepository.java
@@ -1,7 +1,10 @@
 package com.keduit.shop.repository;
 
 import com.keduit.shop.entity.ExchangeReturnRequest;
+import com.keduit.shop.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExchangeReturnRequestRepository extends JpaRepository<ExchangeReturnRequest, Long> {
+
+    ExchangeReturnRequest findByOrder(Order order);
 }
